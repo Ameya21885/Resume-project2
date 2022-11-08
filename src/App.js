1f84cb2c7b5fac66771d8e4668f92d1ea41ff1c5
unknown <ameyathakur21@gmail.com>
2022-11-08 14:37:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import One from "./components/About/One";
+import Two from "./components/About/Two";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>hello</h1> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="about" element={<About />}>
+          <Route path="one" element={<One />} />
+          <Route path="two" element={<Two />} />
+        </Route>
+
+        <Route path="contact" element={<Contact />} />
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Routes>
     </div>
   );
 }
